4c9b033883678a5076dc610444db0ec8bfa9e709
VetsoJoella <153762248+VetsoJoella@users.noreply.github.com>
2024-02-12 16:26:15 +1100

    Create function_login.php
    Si vrai redirection selon le type admin/user

---
+++ function_login.php
@@ -0,0 +1,20 @@
+<?php
+
+	
+	function checkLogin($email, $pwd){
+
+		$connection = dbconnect();
+	    $str = "select * from The_Login where email='$email' and mdp = '$pwd'";
+	    $resultat = mysqli_query($connection, $str);
+
+	    if (mysqli_num_rows($resultat)==0) {
+	        
+	      return "index";
+	    }
+	    $res = mysqli_fetch_assoc($resultat);
+	    return $res['type']."/accueil" ;
+    }
+
+
+
+?>
