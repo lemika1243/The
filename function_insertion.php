5a5aba808e94a8ab2fd88ee2b3964a27dc9239d7
VetsoJoella <153762248+VetsoJoella@users.noreply.github.com>
2024-02-12 17:27:30 +1100

    Create function_insertion.php
    Rename et ajouter de fonction addCueillette

---
+++ function_insertion.php
@@ -0,0 +1,46 @@
+<?php
+
+
+	function insertThe($nom, $occupation, $rendement, $prixVente, $prixAchat){
+
+		$connection = dbconnect();
+		$string = "insert into The_The (nom, occupation,rendement, prixVente, prixAchat) values ('%s', %f, %f, %f,%f)"; 
+	    $query = sprintf($string,$nom, $occupation, $rendement, $prixVente, $prixAchat);
+	    
+	    // echo $query;
+	    if(mysqli_query($connection, $query)){
+	    	return "The inseree";
+	    }
+	    return "The non inseree";
+	}
+
+
+	function insertParcelle($nom, $surface,$idThe){
+
+		$connection = dbconnect();
+		$string = "insert into The_Parcelle (nom, surface, idThe) values ('%s', %f, %d)"; 
+	    $query = sprintf($string,$nom,$surface,$idThe);
+	    
+	    // echo $query;
+	    if(mysqli_query($connection, $query)){
+	    	return "Parcelle inseree";
+	    }
+	    return "Parcelle non inseree";
+	}
+
+	function insertCueilleur ($nom,$dateEmbauche){
+
+		$connection = dbconnect();
+
+		$dateFormat = date('Y-m-d',strtotime(($dateEmbauche)));
+		$string = "insert into The_Cueilleur (nom,dateEmbauche) values ('%s', '%s')"; 
+	    $query = sprintf($string,$nom,$dateFormat);
+	    
+	    // echo $query;
+	    if(mysqli_query($connection, $query)){
+	    	return "Cueilleur inseree";
+	    }
+	    return "Cueilleur non inseree";
+	}
+
+?>
