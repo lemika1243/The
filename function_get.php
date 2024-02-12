0a75ab621cf629705752f1e7c11e0c873d597a71
VetsoJoella <153762248+VetsoJoella@users.noreply.github.com>
2024-02-12 17:58:26 +1100

    Update function_get.php
    Last Salaire

--- function_get.php
+++ function_get.php
@@ -13,4 +13,17 @@
 		return $tab;
 	}
 
+
+	function getSalaire(){
+
+		$connection = dbconnect();
+	    $str = "select * from The_Salaire order by dateSalaire DESC limit 1";	// fonction temporaire prenant le salaire par date max
+	    $resultat = mysqli_query($connection, $str);
+	    
+	    while ($res = mysqli_fetch_assoc($resultat)) {
+	    	return $res  ;
+	    }
+
+	}
+
 ?>
