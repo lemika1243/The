15f8a7881d92e09d562ba7fb5c0428d1200e3891
VetsoJoella <153762248+VetsoJoella@users.noreply.github.com>
2024-02-12 20:35:25 +1100

    Update function_get.php
    Production

--- function_get.php
+++ function_get.php
@@ -38,6 +38,28 @@
 		
 	}
 
+	function getProduction($min , $max){								// 
+
+		$dif = monthNumber($min,$max) ;
+
+		$connection = dbconnect();
+	    $str = "create or replace view v_TheAndCulture as select c.*, idThe, rendement from The_Culture c join The_Parcelle p on idParcelle  = p.id join The_The t on idThe = t.id  ";	
+	    $resultat = mysqli_query($connection, $str);
+		
+		$str = "select * from v_TheAndCulture ";	
+	    $resultat = mysqli_query($connection, $str);
+	    $somme = 0 ;
+
+	    //echo (count(mysqli_num_rows($resultat)));
+	    while ($res = mysqli_fetch_assoc($resultat)) {
+
+	    	$meter = haToMeterSquare($res['surface']);
+	    	$pied = meterSquareToPied($meter);
+	    	$somme = $somme + $res['rendement']*$pied;
+	    }
+	    return  $dif*$somme ;
+	}
+
 	function getParcelle(){
 		$connection = dbconnect();
 	    $str = "select * from The_Parcelle";	// fonction temporaire prenant le salaire par date max
