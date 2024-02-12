6da5bbf7da9c4a51efee52de35d3492acea15b9d
VetsoJoella <153762248+VetsoJoella@users.noreply.github.com>
2024-02-12 17:18:41 +1100

    Create function_get.php
    Getter les listes des The pour l'insert Parcelle (select)

---
+++ function_get.php
@@ -0,0 +1,16 @@
+<?php
+
+
+	function getThe(){
+
+		$connection = dbconnect();
+	    $str = "select * from The_The";
+	    $resultat = mysqli_query($connection, $str);
+	    $tab = array();
+	    while ($res = mysqli_fetch_assoc($resultat)) {
+	    	$tab[] = $res  ;
+	    }
+		return $tab;
+	}
+
+?>
