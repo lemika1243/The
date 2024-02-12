52d5782c4b902f7e9e669344fd1caef17acfe6bb
VetsoJoella <153762248+VetsoJoella@users.noreply.github.com>
2024-02-12 16:42:40 +1100

    Create function_Login.php

---
+++ function_Login.php
@@ -0,0 +1,30 @@
+<?php
+
+	
+	function checkLogin($email, $pwd){
+
+		$connection = dbconnect();
+	    $str = "select * from The_Login where email='$email' and mdp = '$pwd'";
+	    $resultat = mysqli_query($connection, $str);
+
+	    if (mysqli_num_rows($resultat)==0) {
+	        
+	      return "index";
+	    }
+	    $res = mysqli_fetch_assoc($resultat);
+	    return $res['type']."/accueil" ;
+    }
+
+     function getFirstLogin($type){
+
+    	$connection = dbconnect();
+	    $str = "select * from The_Login where type='$type' ";
+	    $resultat = mysqli_query($connection, $str);
+
+	    while ($res = mysqli_fetch_assoc($resultat)) {
+	    	return $res ;
+	    }
+
+    }
+
+?>
